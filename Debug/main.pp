038ed87771072f535db30c6118412493f4b25697
dusts <oneisoneonlyone@foxmail.com>
2016-07-30 06:07:23 +0800

    general IO test

---
+++ Debug/main.pp
@@ -0,0 +1,15 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: C:/Program\ Files\ (x86)/ccs\ mps430/ccsv6/ccs_base/msp430/include/MSP430G2553.h
+main.obj: C:/Program\ Files\ (x86)/ccs\ mps430/ccsv6/ccs_base/msp430/include/in430.h
+main.obj: C:/Program\ Files\ (x86)/ccs\ mps430/ccsv6/tools/compiler/ti-cgt-msp430_4.4.4/include/intrinsics.h
+main.obj: C:/Program\ Files\ (x86)/ccs\ mps430/ccsv6/tools/compiler/ti-cgt-msp430_4.4.4/include/intrinsics_legacy_undefs.h
+main.obj: D:/Users/Administrator/workspace_v6_1/PWM_TEST/src/TA_PWM.h
+
+../main.c: 
+C:/Program\ Files\ (x86)/ccs\ mps430/ccsv6/ccs_base/msp430/include/MSP430G2553.h: 
+C:/Program\ Files\ (x86)/ccs\ mps430/ccsv6/ccs_base/msp430/include/in430.h: 
+C:/Program\ Files\ (x86)/ccs\ mps430/ccsv6/tools/compiler/ti-cgt-msp430_4.4.4/include/intrinsics.h: 
+C:/Program\ Files\ (x86)/ccs\ mps430/ccsv6/tools/compiler/ti-cgt-msp430_4.4.4/include/intrinsics_legacy_undefs.h: 
+D:/Users/Administrator/workspace_v6_1/PWM_TEST/src/TA_PWM.h: 
